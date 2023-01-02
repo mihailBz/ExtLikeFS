2f09578cdabbc6735597424d36d5c3b3c3cc5a75
Mykhailo Briazkalo <mihialstudykpi@gmail.com>
2023-01-02 20:47:15 +0100

    added exceptions

---
+++ exceptions.py
@@ -0,0 +1,18 @@
+class OutOfInodes(Exception):
+    pass
+
+
+class InvalidPath(Exception):
+    pass
+
+
+class FileDoesNotExist(Exception):
+    pass
+
+
+class FileAlreadyExists(Exception):
+    pass
+
+
+class OutOfBlocks(Exception):
+    pass
